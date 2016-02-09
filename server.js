7f4cc79682d0d55c0fd648cfa0b83eb87af20df2
Tom Gray <tom.c.gray@gmail.com>
2016-02-09 01:38:03 +0000

    some extra stuff

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ var app = express();
 app.engine('swig', swig.renderFile);
 app.set('view engine', 'swig');
 app.set('views', __dirname + '/public');
-
+app.set('view cache', false);
 // routes for static files? in what universe is this better than PHP?
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/img', express.static(__dirname + '/public/img'));
 app.use('/js', express.static(__dirname + '/public/js'));
-
+swig.setDefaults({ cache: false });
 // wtf is even happening
 app.listen(port, function () {
 	console.log('Example app listening on port ' + port);
